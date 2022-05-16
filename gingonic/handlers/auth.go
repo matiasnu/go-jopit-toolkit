e9874ddbf78fd466b4a624f60444cb596efb1f89
agustin_rabini <ext_arabinial@falabella.cl>
2022-05-15 23:04:53 -0300

    credentials.json loads from OS env variables, first attempt

--- gingonic/handlers/auth.go
+++ gingonic/handlers/auth.go
@@ -2,8 +2,11 @@ package handlers
 
 import (
 	"context"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"sync"
 
@@ -55,18 +58,57 @@ var (
 	once           sync.Once
 )
 
+type FirebaseCredential struct {
+	Type                    string `json:"type"`
+	ProjectId               string `json:"projectId"`
+	PrivateKeyId            string `json:"privateKeyId"`
+	PrivateKey              string `json:"privateKey"`
+	ClientEmail             string `json:"clientEmail"`
+	ClientId                string `json:"clientId"`
+	AuthUri                 string `json:"authUri"`
+	TokenUri                string `json:"tokenUri"`
+	AuthProviderX509CertUrl string `json:"authProviderX509CertUrl"`
+	ClientX509CertUrl       string `json:"clientX509CertUrl"`
+}
+
+func (fc *FirebaseCredential) loadCredentials() error {
+
+	fc.Type = os.Getenv("TYPE")
+	fc.ProjectId = os.Getenv("PROYECT_ID")
+	fc.PrivateKeyId = os.Getenv("PRIVATE_KEY_ID")
+	fc.PrivateKey = os.Getenv("PRIVATE_KEY")
+	fc.ClientEmail = os.Getenv("CLIENT_EMAIL")
+	fc.ClientId = os.Getenv("CLIENT_ID")
+	fc.AuthUri = os.Getenv("AUTH_URI")
+	fc.TokenUri = os.Getenv("TOKEN_URI")
+	fc.AuthProviderX509CertUrl = os.Getenv("AUTH_PROVIDER_x509_CERT_URL")
+	fc.ClientX509CertUrl = os.Getenv("CLIENT_x509_CERT_URL")
+
+	bytes, err := json.Marshal(fc)
+	if err != nil {
+		return err
+	}
+
+	_ = ioutil.WriteFile("credentials.json", bytes, 0644)
+
+	return nil
+}
+
 type FirebaseClient struct {
 	AuthClient *auth.Client
 }
 
 //initiates the firebase client ONCE
-func Newfirebase() *FirebaseClient {
+func NewfirebaseService() *FirebaseClient {
 	once.Do(InitFirebase)
 
 	return firebaseClient
 }
 
 func InitFirebase() {
+
+	firebaseCredential := FirebaseCredential{}
+	firebaseCredential.loadCredentials()
 
 	opt := option.WithCredentialsFile("credentials.json")
 	app, err := firebase.NewApp(context.Background(), nil, opt)
@@ -85,7 +127,6 @@ func InitFirebase() {
 }
 
 func AuthWithFirebase() gin.HandlerFunc {
-	InitFirebase()
 	return func(c *gin.Context) {
 
 		header := c.GetHeader("HeaderAuthorization")
