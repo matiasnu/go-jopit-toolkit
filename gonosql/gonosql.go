79e4778489a5b5bd93e5fd427c18f114150d70d3
Matias Nuñez <Matiasne45@gmail.com>
2023-02-21 14:52:37 -0300

    Add Get by id method

--- gonosql/gonosql.go
+++ gonosql/gonosql.go
@@ -8,22 +8,28 @@ import (
 )
 
 type Repository interface {
-	InsertOne(storage Data, models interface{}) (*mongo.InsertOneResult, error)
-	GetByFilter(storage Data, filter bson.M) (*mongo.Cursor, error)
+	InsertOne(ctx context.Context, storage Data, models interface{}) (*mongo.InsertOneResult, error)
+	GetByFilter(ctx context.Context, storage Data, filter bson.M) (*mongo.Cursor, error)
+	Get(ctx context.Context, storage Data, id string)
 }
 
-func InsertOne(storage Data, models interface{}) (*mongo.InsertOneResult, error) {
-	result, err := storage.Collection.InsertOne(context.TODO(), models)
+func InsertOne(ctx context.Context, storage Data, models interface{}) (*mongo.InsertOneResult, error) {
+	result, err := storage.Collection.InsertOne(ctx, models)
 	if err != nil {
 		return nil, err
 	}
 	return result, nil
 }
 
-func GetByFilter(storage Data, filter bson.M) (*mongo.Cursor, error) {
-	cursor, err := storage.Collection.Find(context.TODO(), filter)
+func GetByFilter(ctx context.Context, storage Data, filter bson.M) (*mongo.Cursor, error) {
+	cursor, err := storage.Collection.Find(ctx, filter)
 	if err != nil {
 		return nil, err
 	}
 	return cursor, nil
+}
+
+func Get(ctx context.Context, storage Data, id string) *mongo.SingleResult {
+	// TODO add findOneOptions in method
+	return storage.Collection.FindOne(ctx, bson.M{"_id": id})
 }
