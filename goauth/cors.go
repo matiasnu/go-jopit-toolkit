a58fcb7a13f47042498fc1b12903ffdef2b43ae5
agustin_rabini <ext_arabinial@falabella.cl>
2023-07-28 19:32:50 -0300

    cors middleware added

---
+++ goauth/cors.go
@@ -0,0 +1,21 @@
+package goauth
+
+import "github.com/gin-gonic/gin"
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://jopit.com.ar")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Origin, Accept, token")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+		c.Writer.Header().Set("Vary", "Origin")
+		c.Writer.Header().Set("Vary", "Access-Control-Request-Method")
+		c.Writer.Header().Set("Vary", "Access-Control-Request-Headers")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
