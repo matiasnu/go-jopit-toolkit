16106d9daccaba1bf97fffd63462ad27b1f33af4
agustin_rabini <ext_arabinial@falabella.cl>
2023-07-30 20:02:26 -0300

    fixed some issues

--- gingonic/handlers/cors.go
+++ gingonic/handlers/cors.go
@@ -5,31 +5,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-/* func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://jopit.com.ar")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Origin, Accept, token")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
-		c.Writer.Header().Set("Vary", "Origin")
-		c.Writer.Header().Set("Vary", "Access-Control-Request-Method")
-		c.Writer.Header().Set("Vary", "Access-Control-Request-Headers")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		c.Next()
-	}
-}
-*/
-
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		cors.New(cors.Config{
 			AllowOrigins:     []string{"https://jopit.com.ar"},
-			AllowMethods:     []string{"PUT", "PATCH", "GET"},
-			AllowHeaders:     []string{"Origin", "Content-Type", "Origin", "Accept"},
+			AllowMethods:     []string{"PUT", "POST", "GET", "DELETE"},
+			AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
 			ExposeHeaders:    []string{"Content-Length"},
 			AllowCredentials: true,
 		})
