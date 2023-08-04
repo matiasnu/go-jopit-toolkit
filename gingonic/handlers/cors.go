3121ea0ad40c098bdf2014b71b27b20132d9ec51
Matias Nuñez <Matiasne45@gmail.com>
2023-08-04 19:18:40 -0300

    Fix Cors

--- gingonic/handlers/cors.go
+++ gingonic/handlers/cors.go
@@ -1,28 +1,21 @@
 package handlers
 
 import (
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
-var (
-	corsConfig = cors.Config{
-		AllowAllOrigins:  true,
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"PUT", "POST", "GET", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "token"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-	}
-)
-
 func CORSMiddleware() gin.HandlerFunc {
-	// Crea el middleware de CORS utilizando la configuración
-	corsMiddleware := cors.New(corsConfig)
-
 	return func(c *gin.Context) {
-		// Aplica el middleware de CORS a la solicitud actual
-		corsMiddleware(c)
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://jopit.com.ar")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Origin, Accept, token")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+		c.Writer.Header().Set("Vary", "Origin")
+		c.Writer.Header().Set("Vary", "Access-Control-Request-Method")
+		c.Writer.Header().Set("Vary", "Access-Control-Request-Headers")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
 		c.Next()
 	}
 }
