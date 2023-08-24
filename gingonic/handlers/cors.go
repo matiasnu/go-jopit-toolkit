44c53afc888098c147a88c2d5058b36fe0f46708
agustin_rabini <ext_arabinial@falabella.cl>
2023-08-24 20:17:26 -0300

    switched allow originto *

--- gingonic/handlers/cors.go
+++ gingonic/handlers/cors.go
@@ -6,9 +6,9 @@ import (
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://jopit.com.ar")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Origin, Accept, token, authorization, headerauthorization")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Origin, Accept, headerauthorization")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 		c.Writer.Header().Set("Vary", "Origin")
 		c.Writer.Header().Set("Vary", "Access-Control-Request-Method")
 		c.Writer.Header().Set("Vary", "Access-Control-Request-Headers")
